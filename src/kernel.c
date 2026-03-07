d2c27d6e1be59633d56aa8f452828986b68fd882
Axiom-Scratch <258008941+Axiom-Scratch@users.noreply.github.com>
2026-03-08 00:14:27 +0900

    kernel: add VGA text output with welcome message

--- src/kernel.c
+++ src/kernel.c
@@ -1,22 +1,24 @@
 #include <stdint.h>
 
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+#define VGA_COLOR 0x0F
+
 void kernel_main()
 {
-    char* video = (char*)0xB8000;
+    uint16_t* video = (uint16_t*)0xB8000;
 
     // clear screen
-    for(int i = 0; i < 80*25; i++)
+    for(int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++)
     {
-        video[i*2] = ' ';
-        video[i*2+1] = 0x0F;
+        video[i] = (VGA_COLOR << 8) | ' ';
     }
 
     const char* msg = "Hello Kernel, Welcome to Axiom-Kernel";
 
     for(int i = 0; msg[i] != 0; i++)
     {
-        video[i*2] = msg[i];
-        video[i*2+1] = 0x0F;
+        video[i] = (VGA_COLOR << 8) | msg[i];
     }
 
     while(1) {}
