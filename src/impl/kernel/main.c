8aef31eb40ac2696692f24cb6b1b861f8795c3b4
Axiom-Scratch <258008941+Axiom-Scratch@users.noreply.github.com>
2026-03-14 16:36:32 +0900

    Volatile added ;)

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,6 +2,8 @@
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("WELCOME TO OUR 64-bit kernel lol");
+    print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
+    print_str("HELLO PROYA\n");
+    print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    print_str("HOLY MOLY");
 }
